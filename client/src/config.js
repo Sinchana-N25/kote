361813e506f36a9cf040f2b03d18183912ce85aa
Sinchana N <sinchana2505@gmail.com>
2025-12-10 21:16:34 +0530

    deployment ready

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,2 @@
 // client/src/config.js
-export const API_URL =
-  "https://vercel.com/sinchana2505-2366s-projects/kote-server/8obFbXaNWQSzVtr1CMJi7r7XCujM/api";
+export const API_URL = "https://kote-server.vercel.app/";
